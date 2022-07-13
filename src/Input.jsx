29446397f95a2f4d9e889621e7b1799ed63b3e64
Doctor Who <80987591+10ochka@users.noreply.github.com>
2022-07-13 14:12:38 +0300

    Update Input.jsx

--- src/Input.jsx
+++ src/Input.jsx
@@ -2,9 +2,7 @@ import React from "react";
 const Input = React.forwardRef(
     (props, ref) => {
         return (
-                <input className="Input" size={5} ref={ref} {...props}></input>
-
-            
+            <input className="Input" size={5} ref={ref} {...props}></input>
         )
     })
 
