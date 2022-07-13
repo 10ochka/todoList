83685847747c2485693dc48f876858a4a17f4691
Doctor Who <80987591+10ochka@users.noreply.github.com>
2022-07-13 14:13:28 +0300

    Update TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
-import delIcon from "./icons/delIcon.png"
+import Button from "./Button";
+import DelIcon from "./icons/DelIcon.png";
+import EditIcon from "./icons/EditIcon.png"
 
 
 const TodoItem = ({remove, todo}) => {
     const [done, setDone] = useState(false)
+    const [editig, setEditing] = useState(false)
+    const editTodo = e => {
+        e.preventDefault()
+        todo.body = e.target.value
+    }
     return (
         <div className="TodoItem" key={todo.id}>
             <input className="DoneCheckbox" type="checkbox" onChange={() => setDone(!done)}></input>
             <div className="TodoText">
-            {done
-            ? <strike>{todo.body}</strike>
-            : todo.body}
+                {editig
+                 ? <textarea wrap="soft" className="EditTodoField" onChange={editTodo} defaultValue={todo.body}></textarea>
+                 : done
+                 ? <strike>{todo.body}</strike>
+                 : todo.body
+                 }
             </div>
-            <div className="DelIcon">
-                <img   onClick={() => {remove(todo); console.log(todo.id)}} src={delIcon} style={{height: 10, width: 10}}></img>
-            </div>
+
+            <Button 
+                callback={() => setEditing(!editig)} 
+                className="EditButton" src={EditIcon}
+                style={{height: 13, width: 13}}
+            ></Button>
+            <Button 
+                callback={() => remove(todo)} 
+                className="DelButton" src={DelIcon} 
+                style={{height: 10, width: 10}}
+            ></Button>
+
         </div>
         
     )
