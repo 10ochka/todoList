7c7892cbead1816e2c56e2af9ac6a8aca87e0561
Doctor Who <80987591+10ochka@users.noreply.github.com>
2022-07-15 12:48:02 +0300

    Delete Button.jsx

--- src/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Button = ({src = "", callback, style = {}, className="", children}) => {
-    return src
-    ? <img alt="" src={src} onClick={callback} style={style} className={className} ></img>
-    : <button onClick={callback} className={className}>{children}</button>
-}
-export default Button
