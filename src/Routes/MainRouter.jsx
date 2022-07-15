9048e40e8c8252185c2a9fcbb491c5ea2ed0f157
Doctor Who <80987591+10ochka@users.noreply.github.com>
2022-07-15 15:44:27 +0300

    Add files via upload

---
+++ src/Routes/MainRouter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Paths } from "./Routes.js";
+
+
+const MainRouter = () => {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                { Paths.map( route => 
+                    <Route 
+                        key={ route.path } 
+                        path={ route.path } 
+                        element={ route.element }
+                    /> 
+                ) }
+
+                <Route 
+                    path="/*" 
+                    element={ <h1> Page not found </h1> }
+                />
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default MainRouter
