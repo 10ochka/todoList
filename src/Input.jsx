5a92dafc0dc3fdec90350d4bb827314f397e98a4
Doctor Who <80987591+10ochka@users.noreply.github.com>
2022-07-15 12:48:15 +0300

    Delete Input.jsx

--- src/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Input = React.forwardRef(
-    (props, ref) => {
-        return (
-            <input className="Input" size={5} ref={ref} {...props}></input>
-        )
-    })
-
-export default Input
