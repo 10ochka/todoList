3b042b67f14f756e137e3eb305ce5784615f92a9
Doctor Who <80987591+10ochka@users.noreply.github.com>
2022-07-18 13:06:32 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/components/TodoItemList.jsx", 
+            "./src/components/TodoItem.jsx", 
+            "./src/components/Menu.jsx", 
+            "./src/components/Input.jsx",
+            "./src/components/Form.jsx",
+            "./src/components/Button.jsx",
+            "./src/pages/TodoList.jsx",
+            "./src/pages/Calculator.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
