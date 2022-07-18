7e9d87212519bcc23ad52e38be20c6ed1932af9f
Doctor Who <80987591+10ochka@users.noreply.github.com>
2022-07-18 13:07:04 +0300

    Delete src directory

--- src/Routes/Routes.js
+++
@@ -1,8 +0,0 @@
-import Calculator from "../pages/Calculator";
-import TodoList from "../pages/TodoList";
-
-
-export const Paths = [
-    { name: "Todos", path: "/todos", element: <TodoList/> },
-    { name: "Calculator", path: "/calc", element: <Calculator/> }
-]
